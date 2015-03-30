a22f21814d61caa8bbd7f72cd3fbe3cae4c4f17e
bl4ckdu5t <joerexko@gmail.com>
2015-03-30 12:00:17 -0500

    Registration works perfect from landing page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
   	@categories = Category.all.map { |category| [category.name, category.index] }
+  	@user = User.new
   end
   def story
   end
